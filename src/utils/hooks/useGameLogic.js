cb5baee66f1beff4f9179e07b7697f534ff92cab
dante <hemandaney@yahoo.com>
2022-07-06 08:31:39 +0800

    sending latest update

--- src/utils/hooks/useGameLogic.js
+++ src/utils/hooks/useGameLogic.js
@@ -1,6 +1,14 @@
 import React, {useState,useEffect} from 'react'
 
 const useGameLogic = ()=>{
+    const columns = 52
+    const interval= 300
+    const [grid, setGrid] = useState([])
+
+    const initialize =(columns)=>{
+        let cells = newArray(columns)
+        for
+    }
 
 }
 
